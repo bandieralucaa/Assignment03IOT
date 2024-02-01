07a882e91086e2432f79dc4c35670f770711cf08
bandieralucaa <lucabandiera2302@gmail.com>
2024-02-01 18:11:20 +0100

    Add interface for ServoController

--- backend2/app/src/main/java/backend2/serial/main/ServoController.java
+++ backend2/app/src/main/java/backend2/serial/main/ServoController.java
@@ -1,105 +1,20 @@
 package backend2.serial.main;
 
-import backend2.controller.ControllerObs;
-import backend2.controller.ValveType;
-import backend2.serial.comm.CommChannel;
-import backend2.serial.comm.SerialCommChannel;
-import java.util.Arrays;
+public interface ServoController {
 
+    /**
+     * move the servo to the specified degrees
+     * @param degrees
+     */
+    void moveServo(int degrees);
 
-public class ServoController {
-
-    private final boolean SERIAL_D = false;
-    private final static boolean SHOW = false;
-
-    private CommChannel commChannel;
-    private final ControllerObs obs;
-
-    public ServoController(String portName, int baudRate, ControllerObs obs) throws Exception{
-        commChannel = new SerialCommChannel(portName, baudRate);
-        this.obs = obs;
-    }
-
-    public void moveServo(int degrees) {
-        String command = "_v:" + degrees;
-        if (SERIAL_D) {
-            log("MANDO nuova apertura valvola : " + degrees);
-        }
-        commChannel.sendMsg(command);
-    }
+    /**
+     * execute the command or the commands received from the serial
+     */
+    void exec();
+    
+} 
    
-    public void exec() {
-        if (commChannel.isMsgAvailable()){
-            String tmp = "";
-            try {
-                tmp = commChannel.receiveMsg();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            if (SERIAL_D){
-                log("--> " + tmp + "<---");
-            }
-            
-            Arrays.stream(tmp.split("_"))
-                .parallel()
-                .peek(s -> {
-                    if (SERIAL_D) {
-                        System.out.println("-> " + s);
-                    }
-                })
-                .filter(s -> !s.isEmpty())
-                .forEach(s -> {
-                    String[] pairKV = s.split(":");
 
-                    if (pairKV.length == 2){
-                        execCommand(pairKV[0].charAt(0), pairKV[1]);
-                    }
-                    
-                });
-        }
-    }
-
-    private void execCommand(char command, String value) {
-
-        if(SERIAL_D){
-            log("RICEVUTO Comando: " + command + " valore : " + value);
-        }
-
-        switch (command) {
-            case 'v':
-                try {
-                    int a = Integer.parseInt(value);
-                    this.obs.setActValveOp(a);
-                } catch (Exception e) {
-                }
-                
-                break;
-
-            case 's':
-                ValveType tmp = ValveType.UNKNOW;
-                switch (value.charAt(0)) {
-                    case 'A':
-                        tmp = ValveType.AUTOMATIC;
-                        break;
-
-                    case 'M':
-                        tmp = ValveType.MANUAL;
-                        break;
-                
-                    default:
-                        break;
-                }
-                this.obs.setNewValveType(tmp);
-                break;
-        
-            default:
-                break;
-        }
-
-    }
-
-    private void log(String toLog){
-        System.out.println("[SERIAL] " + toLog);
-    }
-}
- 
+    
+   
